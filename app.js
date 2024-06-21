b9bd628c76bfd259bc03331d230006d2f32c8260
Maria Eduarda <mariaeduarda.art18@gmail.com>
2024-06-20 22:16:14 -0300

    Desafios

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Boas-vindas ao jogo do Número Secreto!');
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    prompt('Isso aí! Você descobriu o número secreto (5)');
+}
