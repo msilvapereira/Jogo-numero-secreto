e249b96b87042ecb2882f166136f845b8f9eb46c
Maria Eduarda <85771492+msilvapereira@users.noreply.github.com>
2024-06-21 11:14:15 -0300

    Update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 alert('Boas-vindas ao jogo do Número Secreto!');
 
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    prompt('Isso aí! Você descobriu o número secreto (5)');
+    alert('Isso aí! Você descobriu o número secreto (5)');
+} else {
+    alert('Você errou :(')
 }
